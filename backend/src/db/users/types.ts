a25aff54c9b7f93050a83133814a071567d4b560
Agunbiade Adedeji <dejongbaba@gmail.com>
2024-12-11 17:36:02 +0100

    feat: add Address type

--- backend/src/db/users/types.ts
+++ backend/src/db/users/types.ts
@@ -1,7 +1,20 @@
+import {Address} from "../Addresses/types";
+
 export interface User {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
-  phone: string;
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+}
+
+export interface UserWithAddress extends User {
+    address?: Address;
+}
+
+export interface UsersResponse {
+    users: UserWithAddress[];
+    totalRecords: number;
+    totalPages: number;
+    currentPage: number
 }
