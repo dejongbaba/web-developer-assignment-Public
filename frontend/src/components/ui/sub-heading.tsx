503b96fafc78034bed942dfbbd1ff9db9c63091f
Agunbiade Adedeji <dejongbaba@gmail.com>
2024-12-11 17:36:20 +0100

    feat: add sub heading component

---
+++ frontend/src/components/ui/sub-heading.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {HeadingProp} from "@/lib/definitions";
+
+function SubHeading({email, postCount}: HeadingProp) {
+
+    const postsToText = (posts: number) => {
+        if (posts > 1) {
+            return `${posts} posts`
+        }
+        return `${posts} post`
+    }
+    return (
+        <div className='text-sm font-normal'>
+            <span className='text-muted-foreground'>{email}</span> <span
+            className='font-normal'>&#x2022; {postsToText(postCount)}</span></div>
+    );
+}
+
+export default SubHeading;
