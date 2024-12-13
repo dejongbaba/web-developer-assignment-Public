2d4f366cbf38a98c1ca383ec8404e28c7bd2ec32
Agunbiade Adedeji <dejongbaba@gmail.com>
2024-12-13 15:59:07 +0100

    chore: update posts controller

--- backend/src/db/posts/posts.ts
+++ backend/src/db/posts/posts.ts
@@ -1,25 +1,27 @@
 import {connection} from "../connection";
 import {deletePostTemplate, insertPostTemplate, selectPostsTemplate} from "./query-tamplates";
 import {Post} from "./types";
+import {v4 as uuidv4} from 'uuid';
 import {validatePost} from "../../validators/posts";
 
-export const getPosts = (userId: string): Promise<Post[]> =>
+export const getPosts = (userId: string): Promise<{ count: number, posts: Post[] }> =>
     new Promise((resolve, reject) => {
         connection.all(selectPostsTemplate, [userId], (error, results) => {
             if (error) {
                 reject(error);
             }
-            resolve(results as Post[]);
+            resolve({count: results.length, posts: results as Post[]});
         });
     });
 
 export const addPost = (userId: string, title: string, body: string): Promise<void> => {
     validatePost({userId, title, body});
     return new Promise((resolve, reject) => {
-        const createdAt = new Date().toISOString(); // Set the current timestamp
+        const createdAt = new Date().toISOString();
+        const id = uuidv4().replace(/-/g, '')
         connection.run(
             insertPostTemplate,
-            [userId, title, body, createdAt],
+            [id, userId, title, body, createdAt],
             function (error) {
                 if (error) {
                     reject(error);
